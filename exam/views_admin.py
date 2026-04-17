e9718d0f29a2c6377780eb15d4afb98d5bcdd7d9
Azizbek Azimov <azizbekazimov1v2@gmail.com>
2026-04-17 10:03:54 +0500

    Remove temporary admin creator

--- exam/views_admin.py
+++ exam/views_admin.py
@@ -206,19 +206,3 @@ def delete_video(request, video_id):
         return redirect('admin_panel')
 
     return render(request, 'exam/delete_video.html', {'video': video})
-
-
-def create_admin(request):
-    username = "SATazizbek"
-    email = "azizbekazimov1v2@gmail.com"
-    password = "SATazizbek21."
-
-    if User.objects.filter(username=username).exists():
-        return HttpResponse("Admin already exists")
-
-    User.objects.create_superuser(
-        username=username,
-        email=email,
-        password=password
-    )
-    return HttpResponse("Admin created successfully")
