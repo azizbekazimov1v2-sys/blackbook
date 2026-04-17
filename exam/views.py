018ce340f4d12eda02eae416e8d01f9f5885b23a
Azizbek Azimov <azizbekazimov1v2@gmail.com>
2026-04-17 16:02:15 +0500

    Import secure_video_stream into views

--- exam/views.py
+++ exam/views.py
@@ -4,7 +4,7 @@ from .views_profile import profile_view, edit_profile, leaderboard_view
 from .views_tests import take_pdf_test, submit_pdf_test, check_access, pdf_proxy
 from .views_admin import admin_panel, users_list, give_premium, delete_test, delete_video, create_admin
 from .views_uploads import create_video, upload_sat_pdf, upload_latex_test, upload_pdf_bank, append_pdf_bank
-from .views_career import career_mode_view, career_watch_video, career_test_view
+from .views_career import career_mode_view, career_watch_video, career_test_view, secure_video_stream
 from .views_career_admin import (
     career_manager,
     career_video_create,
